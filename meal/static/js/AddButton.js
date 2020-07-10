8c44a2219e2409380e3f03ba9651ca8d78a9e9b0
Tram <tram.ungtt@gmail.com>
2020-07-10 21:02:50 +0700

    Init django app

---
+++ meal/static/js/AddButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class AddButton extends Component {
+    constructor() {
+
+    }
+    render() {
+        return (
+        <div>
+        </div>
+        );
+    }
+}
+
+export default MyOtherComponent;
